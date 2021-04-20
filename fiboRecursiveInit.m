cbbbce521e6d1c5123f4595abd3235004a04564a
tthomazin <tthomazin2018@gmail.com>
2021-04-20 11:18:17 -0500

    added fiboRecursiveInit

--- fiboRecursiveInit.m
+++ fiboRecursiveInit.m
@@ -1,3 +1,6 @@
-% things
-
-%other thinbgs
+function [f] = fiboRecursiveInit(n)
+global count
+count = 0;
+f = fiboRecursive(n);
+fprintf('It took %d function calls to calculate that the %dth Fibonacci number is %d.\n',count,n,f);
+end
